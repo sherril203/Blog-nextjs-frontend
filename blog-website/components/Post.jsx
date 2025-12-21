2bee6c3d18ac34ee3dd67f25c0444210f4b0fbd2
sherril203 <sherrilkumar2003@gmail.com>
2025-12-21 11:17:59 +0530

    updated

--- blog-website/components/Post.jsx
+++ blog-website/components/Post.jsx
@@ -80,7 +80,7 @@ const Post = () => {
       <h2 className="text-2xl text-center font-bold p-3">Our Posts</h2>
 
       {/* Search Bar */}
-      <div className="flex gap-2 p-2 mx-250">
+      <div className="flex gap-2 p-2 mx-150">
         <input
           type="text"
           ref={inputRef}
